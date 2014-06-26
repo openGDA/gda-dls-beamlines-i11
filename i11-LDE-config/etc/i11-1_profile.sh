2935e5c4aa23f4cce2290bb5baa236445318949c
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2014-06-26 14:56:19 +0100

    BLXI-174: create and develop LDE configuration project implement i11-1 profile
    Task-Url: http://jira.diamond.ac.uk/browse/BLXI-174

--- i11-LDE-config/etc/i11-1_profile.sh
+++ i11-LDE-config/etc/i11-1_profile.sh
@@ -1,6 +1,6 @@
 # i11 beamline profile
 
-export BEAMLINE=i11
+export BEAMLINE=i11-1
 
 if [ -r "/dls_sw/$BEAMLINE/software/gda/config/bin/gda_environment.sh" ]; then
         . "/dls_sw/$BEAMLINE/software/gda/config/bin/gda_environment.sh"
@@ -12,12 +12,3 @@ fi
 
 fi
 
-if [ -r "/dls_sw/$BEAMLINE/software/gda/config/bin/detector_environment.sh" ]; then
-        . "/dls_sw/$BEAMLINE/software/gda/config/bin/detector_environment.sh"
-else
-
-if [ -r "/dls_sw/$BEAMLINE/etc/detector_environment.sh" ]; then
-        . "/dls_sw/$BEAMLINE/etc/detector_environment.sh"
-fi
-
-fi
