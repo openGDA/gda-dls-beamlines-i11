61371d7ea73571595aeb426f005aaaaead7e507a
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2014-06-26 15:06:19 +0100

    allow plot to fail and ensure other processes finish correctly

--- i11-LDE-config/scripts/PSD_Flat_Field_Class.py
+++ i11-LDE-config/scripts/PSD_Flat_Field_Class.py
@@ -165,7 +165,11 @@ class FlatFieldCalibration(ScannableMotionBase):
             print "Sum all scanned raw data into one flat field data file..."
             self.sum_flat_field_file = sumScanRawData(numberofscan)
             #plot and view flat field raw data in SWING GUI
-            plot(RAW,self.sum_flat_field_file)
+            try:
+                plot(RAW,self.sum_flat_field_file)
+            except:
+                print "Plot flat field data from .raw data file failed."
+                print "Unexpected error:", sys.exc_info()[0], sys.exc_info()[1]  # @UndefinedVariable
             
             print "Please check the flat field file for any dead pixels, etc.and check that all the bad channels are in the bed channel list at "+BAD_CHANNEL_LIST
             #apply this flat field correction to PSD in GDA permanently
