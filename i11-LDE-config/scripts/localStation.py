5a03c78f367767663166a49c9007f8cf64eac72d
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2014-07-11 17:13:11 +0100

    add metadata to Mythen detector file.

--- i11-LDE-config/scripts/localStation.py
+++ i11-LDE-config/scripts/localStation.py
@@ -211,16 +211,25 @@ deriv = DeviceDerivativeClass("deriv", "energy", "Ie2", "derivative");
 
 print "-----------------------------------------------------------------------------------------------------------------"
 print "create extra pixium scannables: pixium_PUMode, pixium_BaseExposure, pixium_BaseAcquirePeriod, pixium_EarlyFrames, pixium_TotalCount,pixium_FanSpeed1,pixium_FanSpeed2,pixium_DetectorTemperature"
+from i11utilities import createScannableFromPV, ConstantScannable
 try:
+    pixium_TriggerMode=createScannableFromPV("pixium_TriggerMode", "BL11I-EA-DET-10:CAM:TriggerMode", addToNameSpace=True, getAsString=True, hasUnits=False)
     pixium_PUMode = DisplayEpicsPVClass('pixium_PUMode', 'BL11I-EA-DET-10:CAM:PuMode_RBV', 'PU', '%i')
     pixium_BaseExposure = DisplayEpicsPVClass('pixium_BaseExposure', 'BL11I-EA-DET-10:CAM:AcquireTime_RBV', 's', '%.3f')
     pixium_BaseAcquirePeriod = DisplayEpicsPVClass('pixium_BaseAcquirePeriod', 'BL11I-EA-DET-10:CAM:AcquirePeriod_RBV', 's', '%.3f')
-    pixium_EarlyFrames = DisplayEpicsPVClass('pixium_EarlyFrames', 'BL11I-EA-DET-10:CAM:MotionBlur', 'status', '%.0f')
+    pixium_ExcludeEarlyFrames = createScannableFromPV("pixium_ExcludeEarlyFrames", "BL11I-EA-DET-10:CAM:MotionBlur", addToNameSpace=True, getAsString=True, hasUnits=False)
 
     pixium_TotalCount = DisplayEpicsPVClass('pixium_TotalCount', 'BL11I-EA-DET-10:STAT:Total_RBV', 'count', '%.0f') 
+    pixium_TimeStamp = DisplayEpicsPVClass('pixium_TimeStamp', 'BL11I-EA-DET-10:STAT:TimeStamp_RBV', 'time', '%.3f')
+    
+    pixium_DataType=createScannableFromPV("pixium_DataType", "BL11I-EA-DET-10:CAM:DataType", addToNameSpace=True, getAsString=True, hasUnits=False)
+    pixium_ID = DisplayEpicsPVClass('pixium_ID', 'BL11I-EA-DET-10:STAT:UniqueId_RBV', 'no', '%.0f')
+    pixium_Counter = DisplayEpicsPVClass('pixium_Counter', 'BL11I-EA-DET-10:CAM:ArrayCounter_RBV', 'no', '%.0f')
     pixium_FanSpeed1 = DisplayEpicsPVClass('pixium_FanSpeed1', 'BL11I-EA-DET-10:CAM:DetectorFan1Speed', 'rpm', '%.0f')
     pixium_FanSpeed2 = DisplayEpicsPVClass('pixium_FanSpeed2', 'BL11I-EA-DET-10:CAM:DetectorFan2Speed', 'rpm', '%.0f')
     pixium_DetectorTemperature = DisplayEpicsPVClass('pixium_DetectorTemperature', 'BL11I-EA-DET-10:CAM:DetectorTemperature', 'degree', '%.1f') 
+    x_pixel_size=ConstantScannable("x_pixel_size", 142, unit="um")
+    y_pixel_size=ConstantScannable("y_pixel_size", 142, unit="um")
 except:
     print "cannot create extra pixium scannables"
 
